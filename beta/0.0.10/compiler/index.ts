0bed044b5280e121d00a16480eef03c912c0ecfa
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2024-04-09 20:51:31 +0800

    0.0.10

---
+++ beta/0.0.10/compiler/index.ts
@@ -0,0 +1,44 @@
+import StringLiteral from "../ast/string_literal"
+import Expression from "../ast/expression"
+import Program from "../ast/program"
+import get_item from "../utils/get_item"
+import DartCompiler from "./dart"
+import JavaScriptCompiler from "./js"
+import { HydroScriptCompileError } from "../error"
+
+class Compiler {
+    public static languages = [
+        "javascript",
+        "dart"
+    ]
+
+    public program: Program
+    public compiler: JavaScriptCompiler | DartCompiler
+    
+    public compile(program: Program): string {
+        this.program = program
+        
+        const option: Expression = (get_item(program.options.value, "to") ?? {
+            object: "Expression",
+            kind: "StringLiteral",
+            mark: "\"",
+            value: "javascript"
+        } as StringLiteral)
+        if (option.kind != "StringLiteral") throw new HydroScriptCompileError("\"to\" option is not a StringLiteral")
+        
+        const final_option = option as StringLiteral
+        final_option.value = final_option.value.toLowerCase()
+        if (!Compiler.languages.includes(final_option.value)) throw new HydroScriptCompileError("\"to\" option is not valid")
+
+        switch (final_option.value) {
+            case "javascript": {
+                this.compiler = new JavaScriptCompiler()
+                break
+            }
+            default:
+                throw new HydroScriptCompileError("Language not implemented")
+        }
+
+        return this.compiler.compile(this.program)
+    }
+}
---
+++ beta/0.0.10/repl.ts
@@ -0,0 +1,60 @@
+import * as fs from "node:fs"
+import { resolve } from "path"
+
+import Compiler from "./compiler/js"
+import Parser from "./parser"
+
+import { compile } from "."
+
+import prompt from "./utils/prompt"
+
+async function repl(): Promise<void> {
+    const path = process.argv[2]
+    const output_path = process.argv[3]
+
+    const parser = new Parser()
+    const compiler = new Compiler()
+
+    if (path) {
+
+        const run_path = (path: string) => {
+            const input = fs.readFileSync(path, "utf-8")
+            return compile(input)
+        }
+
+        const absolute_path = resolve(path)
+        if (!output_path) console.log(`\nCompiling ${absolute_path}\n`)
+
+        if (!fs.existsSync(absolute_path)) throw new Error(`Cannot find file in path ${absolute_path}`)
+
+        const data = fs.statSync(absolute_path)
+        if (data.isFile()) {
+            const compiled = run_path(absolute_path)
+            if (output_path) {
+                if (fs.existsSync(output_path)) {
+                    const confirmation = await prompt("There is already a directory/file there, are you sure you want to continue? (y/n) ")
+                    if (confirmation == "n") return
+                }
+                fs.writeFileSync(output_path, compiled, { "encoding": "utf-8" })
+            } else console.log(compiled)
+        } else throw new Error(`Path ${absolute_path} is not a file`)
+
+        return
+
+    }
+
+    console.log("\nType \".exit\" to exit")
+    while (true) {
+        let CODE: string
+        while (!CODE) CODE = await prompt("> ")
+        if (CODE == ".exit") break
+
+        try {
+            console.log(compile(CODE))
+        } catch (exception) {
+            console.log(exception)
+        }
+    }
+}
+
+export default repl
