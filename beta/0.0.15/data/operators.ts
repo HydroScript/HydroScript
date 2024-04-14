737a50564663076fec19af3fe80772d061b5c4ed
HyperKNF <115628312+hyperknf@users.noreply.github.com>
2024-04-14 21:44:15 +0800

    0.0.15

---
+++ beta/0.0.15/data/operators.ts
@@ -0,0 +1,91 @@
+import { TokenType } from "../lexer/token_type"
+
+export default {
+    "+": TokenType.BinaryOperator,
+    "-": TokenType.BinaryOperator,
+    "*": TokenType.BinaryOperator,
+    "/": TokenType.BinaryOperator,
+    "**": TokenType.BinaryOperator,
+    "%": TokenType.BinaryOperator,
+
+    "++": TokenType.ArithmeticAssignment,
+    "--": TokenType.ArithmeticAssignment,
+    
+    "[": TokenType.OpenSquareBracket,
+    "]": TokenType.CloseSquareBracket,
+    "{": TokenType.OpenCurlyBracket,
+    "}": TokenType.CloseCurlyBracket,
+
+    ",": TokenType.Comma,
+    ".": TokenType.Dot,
+    
+    "(": TokenType.OpenParenthesis,
+    ")": TokenType.CloseParenthesis,
+    "?": TokenType.QuestionMark,
+    "#?": TokenType.ShortIfOperator,
+    "!?": TokenType.Unless,
+    "#!": TokenType.ShortUnlessOperator,
+    "-?": TokenType.While,
+    ":": TokenType.Colon,
+
+    "<": TokenType.ComparisonOperator,
+    ">": TokenType.ComparisonOperator,
+    "<=": TokenType.ComparisonOperator,
+    "==": TokenType.ComparisonOperator,
+    ">=": TokenType.ComparisonOperator,
+    "!=": TokenType.ComparisonOperator,
+    "===": TokenType.ComparisonOperator,
+    "!==": TokenType.ComparisonOperator,
+
+    "=": TokenType.AssignmentOperator,
+    "+=": TokenType.AssignmentOperator,
+    "-=": TokenType.AssignmentOperator,
+    "*=": TokenType.AssignmentOperator,
+    "/=": TokenType.AssignmentOperator,
+    "%=": TokenType.AssignmentOperator,
+    "**=": TokenType.AssignmentOperator,
+    "<<=": TokenType.AssignmentOperator,
+    ">>=": TokenType.AssignmentOperator,
+    ">>>=": TokenType.AssignmentOperator,
+    "&=": TokenType.AssignmentOperator,
+    "|=": TokenType.AssignmentOperator,
+    "^=": TokenType.AssignmentOperator,
+    "&&=": TokenType.AssignmentOperator,
+    "||=": TokenType.AssignmentOperator,
+    "??=": TokenType.AssignmentOperator,
+
+    "&&": TokenType.LogicalOperator,
+    "||": TokenType.LogicalOperator,
+    "??": TokenType.LogicalOperator,
+
+    "!": TokenType.LogicalNot,
+    "~": TokenType.BitwiseNot,
+
+    "&": TokenType.BitwiseOperator,
+    "|": TokenType.BitwiseOperator,
+    "^": TokenType.BitwiseOperator,
+    "<<": TokenType.BitwiseOperator,
+    ">>": TokenType.BitwiseOperator,
+    ">>>": TokenType.BitwiseOperator,
+
+    ";": TokenType.TypeOf,
+    "@": TokenType.InstanceOf,
+
+    "#": TokenType.Import,
+
+    "->": TokenType.Await,
+    ">>-": TokenType.AsyncFunction,
+    ">-": TokenType.Function,
+
+    "<-": TokenType.Return,
+
+    "\"": TokenType.QuotationMark,
+    "'": TokenType.QuotationMark,
+
+    "::": TokenType.Class,
+    "*>": TokenType.Static,
+    "@@": TokenType.New,
+    "/\\": TokenType.Void,
+
+    ";;": TokenType.Throw
+}
